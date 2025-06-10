4484df3fb914d36bc28eb88c5dda9fdb238f1663
vodnalasharan <123161512+vodnalasharan@users.noreply.github.com>
2025-06-10 12:12:04 +0530

    Delete Inventory_Management directory

--- Inventory_Management/src/main/java/com/example/inventorymanagement/repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.inventorymanagement.repository;
-
-import com.example.inventorymanagement.model.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
